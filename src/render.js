a1b76cb12c356f3667ca2386125a4e6ef8abbb5d
William Chargin <wchargin@gmail.com>
2022-10-06 18:12:53 -0700

    render: use V8 VMs to isolate global state pollution
    No statistically significant effect on performance.

--- src/render.js
+++ src/render.js
@@ -1,8 +1,13 @@
+const fs = require("fs");
+const vm = require("vm");
+
 const jsdom = require("jsdom");
 
 // The `canvas` module needs to be installed for rendering to work, or else
 // it'll fail with a late run-time error. Check here and fail fast.
 require("canvas");
+
+const { default: qql } = require("./vendor/qql.min.js");
 
 async function render({
   seed,
@@ -10,40 +15,40 @@ async function render({
 }) /*: Promise<{ imageData: ArrayBuffer, renderData: Object }> */ {
   const dom = new jsdom.JSDOM("<!DOCTYPE html>");
   const window = dom.window;
-  global.window = window;
-  global.document = window.document;
-  global.screen = window.screen;
-  global.navigator = window.navigator;
+  const container = window.document.createElement("div");
 
-  const container = document.createElement("div");
-  const p5 = require("p5");
-  const { default: qql } = require("./vendor/qql.min.js");
+  // Run in a V8 context because p5.js wants to read from and write to global
+  // state, and this way we contain the pollution.
+  const ctx = vm.createContext(window);
+  vm.runInContext(await moduleSource("p5"), ctx, { filename: "p5.min.js" });
+  window.container = container;
+  window.render = () =>
+    new Promise((resolve, reject) => {
+      new window.p5((p5) => {
+        const pause = () => {}; // don't worry about blocking the main thread; ~5% faster
+        const verbose = false;
+        qql(p5, width, pause, verbose);
 
-  const renderData = await new Promise((resolve, reject) => {
-    new p5((p5) => {
-      const pause = () => {}; // don't worry about blocking the main thread; ~5% faster
-      const verbose = false;
-      qql(p5, width, pause, verbose);
+        const setup = p5.setup;
+        p5.setup = () => {
+          try {
+            setup();
+          } catch (e) {
+            reject(e);
+          }
+        };
 
-      const setup = p5.setup;
-      p5.setup = () => {
-        try {
-          setup();
-        } catch (e) {
-          reject(e);
-        }
-      };
-
-      const draw = p5.draw;
-      p5.draw = async () => {
-        try {
-          resolve(await draw(seed));
-        } catch (e) {
-          reject(e);
-        }
-      };
-    }, container);
-  });
+        const draw = p5.draw;
+        p5.draw = async () => {
+          try {
+            resolve(await draw(seed));
+          } catch (e) {
+            reject(e);
+          }
+        };
+      }, container);
+    });
+  const renderData = await vm.runInContext("render()", ctx);
 
   const canvas = container.getElementsByTagName("canvas")[0];
   if (canvas == null) {
@@ -58,6 +63,12 @@ async function render({
   const imageData = Buffer.from(imageArrayBuffer);
 
   return { imageData, renderData };
+}
+
+async function moduleSource(module) {
+  return await fs.promises
+    .readFile(require.resolve(module))
+    .then((buf) => buf.toString());
 }
 
 module.exports = render;
