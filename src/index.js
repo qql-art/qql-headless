05f69ab131e6412750d869e5754eb8b2b4bdedd6
William Chargin <wchargin@gmail.com>
2022-10-06 18:28:32 -0700

    cli: make seed optional, defaulting to random

--- src/index.js
+++ src/index.js
@@ -5,22 +5,30 @@ const render = require("./render");
 const traitsLib = require("./vendor/qql-traits.min.js");
 
 async function main(args) {
-  const [outdir, seed] = args;
-  if (outdir == null || seed == null) {
-    throw new Error("usage: render <outdir> <seed>");
+  const [outdir, seed = randomSeed(), extraArg] = args;
+  if (outdir == null || extraArg != null) {
+    throw new Error("usage: render <outdir> [<seed>]");
   }
+  console.log("Seed:", seed);
+  const traits = traitsLib.extractTraits(seed);
+  console.log("Traits:", JSON.stringify(traits, null, 2));
 
   const { imageData, renderData } = await render({ seed, width: 2400 });
   const basename = `${new Date().toISOString()}-${seed}.png`;
   const outfile = path.join(outdir, basename);
   await fs.promises.writeFile(outfile, imageData);
 
-  const traits = traitsLib.extractTraits(seed);
-
-  console.log("Seed:", seed);
-  console.log("Traits:", JSON.stringify(traits, null, 2));
   console.log("Render data:", JSON.stringify(renderData, null, 2));
   console.log("Image:", outfile);
+}
+
+function randomSeed() {
+  const buf = Buffer.from(
+    Array(32)
+      .fill()
+      .map(() => Math.random() * 256)
+  );
+  return "0x" + buf.toString("hex");
 }
 
 main(process.argv.slice(2)).catch((e) => {
